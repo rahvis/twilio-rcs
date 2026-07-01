5a57b7fae7c7c4c7a8826e0aa84dec60ef8e9eba
parkshub <AndrewJHPark@protonmail.com>
2026-07-01 16:27:22 -0700

    Force RCS category consent template

--- src/services/twilio.service.ts
+++ src/services/twilio.service.ts
@@ -30,7 +30,7 @@ class TwilioService {
     this.fromNumber = config.TWILIO_PHONE_NUMBER;
     this.authToken = config.TWILIO_AUTH_TOKEN;
     this.contentSids = {
-      consent: config.RCS_CONTENT_CONSENT_SID || DEFAULT_RCS_CONSENT_CONTENT_SID,
+      consent: DEFAULT_RCS_CONSENT_CONTENT_SID,
       language: config.RCS_CONTENT_LANGUAGE_SID,
       role: config.RCS_CONTENT_ROLE_SID,
       location: config.RCS_CONTENT_LOCATION_SID,
